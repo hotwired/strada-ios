5ee2ebb44952616f09e5878a96889864f57ac62a
Zach Waugh <zwaugh@gmail.com>
2020-12-15 15:20:00 -0500

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "Strada",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "Strada",
+            targets: ["Strada"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Strada",
+            dependencies: [],
+            path: "Source",
+            exclude: ["Info.plist"],
+            resources: [
+                .copy("strada.js")
+            ])
+    ]
+)
