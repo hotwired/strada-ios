b342f0bc2a68d2d8f0e5bf882c84c7b80a768256
Fernando Olivares <fernando.olivares@me.com>
2021-09-20 22:52:07 -0500

    spm-bundle-access: Added assertion failure

--- Source/Bridge.swift
+++ Source/Bridge.swift
@@ -98,13 +98,17 @@ public final class Bridge {
 
     private func makeUserScript() -> WKUserScript? {
         guard
-            let path = PathLoader().pathFor(name: "strada", fileType: "js"),
-            let url = URL(string: path),
-            let source = try? String(contentsOf: url, encoding: .utf8) else {
+            let path = PathLoader().pathFor(name: "strada", fileType: "js") else {
                 return nil
         }
-
-        return WKUserScript(source: source, injectionTime: .atDocumentStart, forMainFrameOnly: true)
+        
+        do {
+            let source = try String(contentsOfFile: path)
+            return WKUserScript(source: source, injectionTime: .atDocumentStart, forMainFrameOnly: true)
+        } catch {
+            assertionFailure("Could not open strada.js: \(error)")
+            return nil
+        }
     }
 
     // MARK: - JavaScript Evaluation
