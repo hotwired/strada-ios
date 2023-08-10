745b117e373f0eb9171e078333afa1a930b94417
Denis Švara <denis.svara@gmail.com>
2023-08-10 16:24:06 +0200

    Move user agent substring generation from `Bridge` to `Strada`.

---
+++ Tests/UserAgentTests.swift
@@ -0,0 +1,15 @@
+import Foundation
+import XCTest
+@testable import Strada
+
+class UserAgentTests: XCTestCase {
+    func testUserAgentSubstringWithTwoComponents() {
+        let userAgentSubstring = Strada.userAgentSubstring(for: [OneBridgeComponent.self, TwoBridgeComponent.self])
+        XCTAssertEqual(userAgentSubstring, "bridge-components: [one two]")
+    }
+    
+    func testUserAgentSubstringWithNoComponents() {
+        let userAgentSubstring = Strada.userAgentSubstring(for: [])
+        XCTAssertEqual(userAgentSubstring, "bridge-components: []")
+    }
+}
